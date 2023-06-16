1deb5e1221c23f486b106a9a71e8e41aa84f9285
tongxushengGithub <61938501+tongxushengGithub@users.noreply.github.com>
2023-06-16 22:01:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '我的朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '072aecc4e441770f527b8449e902a558',
+      id: 'gh_e9b74d02af6c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Y4i1Z5IVCS6JpybOvooiEqYG6U8-4G9Oaq85DQvnKq4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
